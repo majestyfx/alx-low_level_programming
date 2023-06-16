6725576355561225b25d553f7d4df7ef7480afc0
root <you@example.com>
2023-06-16 15:13:31 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>/
-#include <errno.h>
 /**
- * main - A C program that prints exactly and that piece of art is useful
+ * main - A c  program that prints exactly and that piece of art is useful
  * Return 1 (Success)
  */
-
-int main(void)
+int main (void);
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
-return (1)
+return (1);
 }
