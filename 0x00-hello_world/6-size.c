af2164f07854315c6b92552f083b0967e72eb4a9
root <you@example.com>
2023-06-15 21:25:28 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,26 @@
  */
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of unsigned_char: %zu byte(s)\n", sizeof(unsigned char));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
-printf("Size of unsigned_short: %zu byte(s)\n", sizeof(unsigned short));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long: %zu byte(s)\n", sizeof(long));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of unsigned_char: %lu byte(s)\n", sizeof(unsigned char));
+printf("Size of short: %lu byte(s)\n", sizeof(short));
+printf("Size of unsigned_short: %lu byte(s)\n", sizeof(unsigned short));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of double: %lu byte(s)\n", sizeof(double));
+printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b);
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
