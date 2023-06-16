4c7ff76de101d82f196c13c860fa9e40972ed326
root <you@example.com>
2023-06-16 14:59:48 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>/
+#include <errno.h>
+/**
+ * main - A C program that prints exactly and that piece of art is useful
+ * Return 1 (Success)
+ */
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+return (1)
+}
