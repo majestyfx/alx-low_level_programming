02c3fb965be25bd2ffbfad42be5719c356a1c836
root <you@example.com>
2023-06-15 21:28:15 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,15 +11,6 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of unsigned_char: %lu byte(s)\n", sizeof(unsigned char));
-printf("Size of short: %lu byte(s)\n", sizeof(short));
-printf("Size of unsigned_short: %lu byte(s)\n", sizeof(unsigned short));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long: %lu byte(s)\n", sizeof(long));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
-printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b);
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
