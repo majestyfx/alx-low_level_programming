3027b1d2bc1e5390bd1b4bf8937d60bc750b80ac
root <you@example.com>
2023-06-17 21:04:24 -0700

    main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints exactly and that piece of art is useful
+ * main - a program that prints
  *
- * Return 1 (Success)
+ * Return 1 (success)
  */
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-}
