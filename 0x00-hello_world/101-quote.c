e617880645fd8b82ca6375a72699f172c11fbd45
root <you@example.com>
2023-06-17 21:25:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 /**
  * main - a code that writes
  * Return 1 (Success)
  */
 int main(void)
+{
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+}
