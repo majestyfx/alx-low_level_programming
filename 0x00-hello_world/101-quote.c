38931c23ec20f5bce6dd143db685a74ad0826c26
root <you@example.com>
2023-06-18 18:38:25 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
 #include <sys/syscall.h>
+/*
+ * main - A part of the code
+ * return 1 (success)
+ */
+int main(int argc, char *argc[])
+{
 
-int main(void) {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, 42);
-    return 1;
+const char ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
 
