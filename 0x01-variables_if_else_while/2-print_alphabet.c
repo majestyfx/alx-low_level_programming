f81962ca45ec5163afe7630160cb4f59f2a557f5
root <you@example.com>
2023-06-19 05:50:45 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
  * main - print capital alphabets
- * Return: 0 (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-{
 putchar(letter);
-}
 putchar('\n');
 return (0);
 }
-
