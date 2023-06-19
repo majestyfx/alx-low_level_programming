88638e9be28c3cfc8c91fc6a3709298908316e4f
root <you@example.com>
2023-06-18 19:31:03 -0700

    last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - evaluate sum of numbers
+ * return (0)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand();
+int lastDigit = n % 10;
+printf("Last digit of %d is ", n);
+if (lastDigit > 5)
+{
+printf("%d and is greater than 5\n", lastDigit);
+}
+else if (lastDigit == 0)
+{
+printf("%d and is 0\n", lastDigit);
+}
+else
+{
+printf("%d and is less than 6 and not 0\n", lastDigit);
+}
+return (0);
+}
