c5c1886c782cd716b7ce7d26c4d433a817f89146
root <you@example.com>
2023-06-18 18:18:32 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,8 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-/**
- * main - a quote that
- * Return 1 (succes)
- */
-int main(void)
-{
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-if (fd != -1)
-return (1);
+#include <sys/syscall.h>
+
+int main(void) {
+    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    return 1;
 }
 
---
+++ 0x00-hello_world/103-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * main - a quote that
+ * Return 1 (succes)
+ */
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
+
