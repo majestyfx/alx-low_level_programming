d784eb2b006ebf9b15fef8aa1befc809ae8e4515
root <you@example.com>
2023-06-19 08:41:17 -0700

    print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * main - print letters in reverse starting from z
  * Return: 0 (success)
  */
 int main(void)
 {
-char letter = 'z';
-for (letter = 'z'; letter >= 'a'; letter++)
-putchar(letter);
-letter--;
+char let;
+for (let = 'z'; let >= 'a'; let--)
+putchar(let);
+let--;
 putchar('\n');
 return (0);
 }
