aa87e7daa88ac653ca92cf6bf6edd29e0832c0dc
root <you@example.com>
2023-06-18 21:23:05 -0700

    posionegetive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("positive\n");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("zero\n");
+printf("%d is zero\n", n);
 }
 else
 {
-printf("negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
