31f640929a4a36badfd955ab178673e66668b8b6
root <you@example.com>
2023-06-19 13:37:54 -0700

    main

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,16 +9,22 @@ int main(void)
 {
 int j, k, l;
 for (j = 10; j < 10; j++)
+{
 for (k = 10; k < 10; k++)
+{
 for (l = 10; l < 10; l++)
+{
+if (j != 7 || k != 8 || l != 9)
 {
 putchar('0' + j);
 putchar('0' + k);
 putchar('0' + l);
-if (j != 7 || k != 8 || l != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
+}
 }
 }
 putchar('\n');
