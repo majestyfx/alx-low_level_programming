6ce42ccec9cb1b60339e95a8f8ea6d40d6c1c4c5
root <you@example.com>
2023-06-19 15:15:48 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 100; i++)
+for (i = 0; i < 98; i++)
 {
-for (j = i; j < 100; j++)
+for (j = i; j < 99; j++)
 {
 putchar('0' + (i / 10));
 putchar('0' + (i % 10));
