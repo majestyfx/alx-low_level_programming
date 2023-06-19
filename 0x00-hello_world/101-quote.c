4923c368ba6dd9d58a716a8011c0326401ac1676
root <you@example.com>
2023-06-18 18:20:44 -0700

    main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 
 int main(void) {
     const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, STDERR_FILENO, message, 42);
     return 1;
 }
 
