e41b773d144fbd42636ca8233586cf24a366ec87
root <you@example.com>
2023-06-21 06:41:20 -0700

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,9 @@ void print_alphabet_x10(void)
 char lets = 'a';
 int count = 0;
 for (lets = 'a'; lets <= 'z'; lets++)
-while (count < 10) count++;
-{
+while (count < 10)
+count++;
+}
 _putchar(lets);
 }
 _putchar('\n');
