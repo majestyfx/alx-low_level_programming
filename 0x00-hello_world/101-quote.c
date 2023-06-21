086b7ce3be357915965f95f523936075454ca0b8
root <you@example.com>
2023-06-20 21:35:30 -0700

    ;;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <sys/syscall.h>
 /*
  * main - A part of the code that creates the text
- * Return: 1 (success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 syscall(SYS_write, STDERR_FILENO, message, 42);
 return (1);
 }
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - print putchar
+ * Returm: 0 (success)
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u')
+putchar('t')
+putchar('c')
+putchar('h')
+putchar('a')
+putchar('r')
+putchar('\n')
+return (0);
+}
