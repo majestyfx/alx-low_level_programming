35413295069b8c4264729eafbc6e412e1b66172f
root <you@example.com>
2023-06-21 12:55:50 -0700

    islow

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _islower - print lower case
+ * c! the charcter checked
+ * Return: 1 lowercase or 0 others
+ */
+int _islower(int c)
+{
+if (c >= 97 && c == 123)
+{
+return (1);
+}
+return (0);
+}
