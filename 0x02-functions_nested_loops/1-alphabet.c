b44a168944ec3813f07552b48a7567125d181e31
root <you@example.com>
2023-06-21 05:34:48 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - make alphabets
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char lets;
+for (lets = 'a'; lets <= 'z'; lets++)
+{
+_putchar(lets);
+}
+_putchar('\n');
+}
+
