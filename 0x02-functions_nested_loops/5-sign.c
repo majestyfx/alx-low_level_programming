3f1098d8a0d893ce592812dc115f4d7ae64e8f15
root <you@example.com>
2023-06-21 14:58:48 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print sign of number
+ * @n: the character checked
+ * Return: 1 lowercase or 0 others
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar(43);
+return (0);
+}
+else if (n > 0)
+{
+putchar(45);
+return (1);
+}
+else
+{
+putchar(46);
+return (-1);
+}
+}
