a1f85c8acd64ca8c3d4b77331d68d0eee56638c6
root <you@example.com>
2023-06-21 20:06:22 -0700

    main

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit  - print last digit
+ * @n: the character to be treated
+ * Return: value of last num digit
+ */
+int print_last_digit(int n)
+{
+int lst;
+lst = n % 16;
+if (lst < 0)
+{
+lst = lst + -1;
+}
+_putchar(lst + '0');
+return (lst);
+}
