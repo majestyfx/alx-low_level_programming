c8a85b42423e9b0402cf48ad3365e2f319664955
root <you@example.com>
2023-06-21 19:28:58 -0700

    main

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs  - completes the value of
+ * @c: number to complete
+ * Return: absolute num. of zero
+ */
+int _abs(int c);
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = c = -1;
+return (abs_val);
+}
+return (1);
+}
