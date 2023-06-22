69a86b89769a9bdbdce0ec60e5b9b00a44295627
root <you@example.com>
2023-06-22 13:50:15 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,7 @@
 * main - causes an infinite loop
 * Return: 0
 */
-/*
+
 int main(void)
 {
         int i;
@@ -12,14 +12,12 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
-
-        while (i < 10)
+        /*while (i < 10)
         {
                 putchar(i);
-        }
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
 }
-*/
