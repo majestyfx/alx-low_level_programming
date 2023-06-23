70a05726c0f95f8f18d76175fb6b9338138b645b
root <you@example.com>
2023-06-23 06:00:40 -0700

    sizes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print square in sizes
+ * @size: character to print
+ * Return: empty
+ */
+
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
