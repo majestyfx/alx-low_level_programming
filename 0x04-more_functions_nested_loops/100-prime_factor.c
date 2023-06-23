3ac8fe0af17f6023e60894cec472273148af83e6
root <you@example.com>
2023-06-23 12:28:36 -0700

    immi

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <nath.h>
+#include <stdio.h>
+
+/**
+ * main - print the largest prime factor
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+long i, maxf
+long number = 612852475143;
+double square = sqrt(number);
+
+for (i = 0; i <= square; i++)
+{
+if (number % x == 0)
+{
+maxf = number / i;
+}
+}
+printf("%ld\n", maxf);
+return (0);
+}
