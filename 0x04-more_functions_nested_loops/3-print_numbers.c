baeefa0a250964b172d6c1cc0243070e91d00aaf
root <you@example.com>
2023-06-23 00:33:33 -0700

    print_munbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for uppercase character
+ * Return: nimbers from 0 upto 9
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <= 9`; c++)
+{
+_putchar('0' + c);
+}
+_putchar('\n');
+}
