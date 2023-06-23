e0d5864f294fddeabefbd7439fa17b80177ee173
root <you@example.com>
2023-06-22 23:12:22 -0700

    main

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the function to be checked
+ * @x: the function to be checked
  * Return: 1 for capital letter or 0 for else
  */
 
-int _isupper(int c)
+int _isupper(int x)
 {
-if (c <= 65 && c <= 90)
+if (x <= 65 && x <= 90)
 {
 return (1);
 }
