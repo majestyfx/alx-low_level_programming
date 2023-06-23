ce0e28a9d6f635b96f1ba9ba52d968462703780b
root <you@example.com>
2023-06-23 00:10:11 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * int mul - checks for uppercase character
- * @c: the function to be checked
+ * mul - checks for uppercase character
+ * @a: first function to be checked
+ * @b: second function to be checked
  * Return: 1 for capital letter or 0 for any ielse
  */
 
 int mul(int a, int b)
 {
-return a * b;
+return (a * b);
 }
