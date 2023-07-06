579222361cce8e9080bb039d1076e4f8c1f4258a
root <you@example.com>
2023-07-05 21:33:19 -0700

    _print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _print_rev_recursion - print string in reverse
+ *  @s: string to print
+ *
+ *  Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0'
+	{
+	return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
