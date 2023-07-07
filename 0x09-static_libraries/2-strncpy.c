561aacd2e732e54c53e6ddb9a17ba4df7aefcf97
root <you@example.com>
2023-07-07 06:28:50 -0700

    libmy.a

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -6,5 +6,5 @@
  
 char *_strncpy(char *dest, char *src, int n)
 {
-return strncpy9dest, src, n);
+return strncpy(dest, src, n);
 }
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h.
+#include <string.h>
 
 int _isalpha(int c)
 {
