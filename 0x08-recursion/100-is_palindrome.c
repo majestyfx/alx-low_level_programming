c7e6bf4e2abae7dd8bd5397ce875f75db9d73b48
root <you@example.com>
2023-07-07 01:18:46 -0700

    is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,35 +9,34 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\'0)
+	if (*s == '\0')
 		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
+
 /**
- * comparator - compares each character
+ * comparator - compare each character of string
  * @s: string
- * @n1: smallest interator
- * @n2: largest interator
+ * @n1: smallest string
+ * @n2: biggest string
  * Return: .
  */
-
 int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + comparators(s, n1 +1, n2 -1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
 /**
- * is_palindrome -detect if dtring is
+ * is_palindrome - detect if dtring is
  * @s: string
- * Return: 1 if s is , or ) if not
+ * Return: 1 if s is , or 0 if not
  */
-
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
