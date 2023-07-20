b0a7ebbaec66f4d58af57f4a0d7a60b0c0595a60
root <you@example.com>
2023-07-20 15:29:41 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -26,8 +26,7 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			}
-		j++;
+			} j++;
 		}
 		switch (format[i])
 		{
@@ -49,8 +48,7 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-		}
-		i++;
+		} i++;
 	}
 	printf("\n"), va_end(valist);
 }
